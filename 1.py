d08c6167ce5a045a8c684f520efb0aa66038e89d
Alve Svarén <alve@hotmail.se>
2020-12-01 06:18:32 +0100

    The code now uses a set instead of tuple and removed some debug output

--- 1.py
+++ 1.py
@@ -1,12 +1,11 @@
 import aoc
 
-data = tuple(map(int, aoc.get_input(1).splitlines()))
+data = {*map(int, aoc.get_input(1).splitlines())}
 
 def part1():
     for num1 in data:
         for num2 in data:
             if num1 + num2 == 2020:
-                print(num1, num2)
                 return num1 * num2
 
 def part2():
@@ -14,7 +13,6 @@ def part2():
         for num2 in data:
             for num3 in data:
                 if num1 + num2 + num3 == 2020:
-                    print(num1, num2)
                     return num1 * num2 * num3
 
 print("Part 1:", part1())

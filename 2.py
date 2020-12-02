52502d86191f0d068cc20f8279d1b63219e145c1
Alve Svarén <alve@hotmail.se>
2020-12-02 06:26:04 +0100

    Formatting

--- 2.py
+++ 2.py
@@ -10,10 +10,10 @@ for password in data:
     minc, maxc, letter, passwd = re.match(
         r"(\d+)-(\d+) ([a-z]): ([a-z]+)", password).groups()
     minc, maxc = int(minc), int(maxc)
-    
+
     if passwd.count(letter) >= minc and passwd.count(letter) <= maxc:
         count1 += 1
-    
+
     if ((passwd[minc-1] == letter) is not (passwd[maxc-1] == letter)):
         count2 += 1
 

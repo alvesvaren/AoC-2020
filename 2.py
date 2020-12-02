1e3c9937ed8648a8b87d70293b5ae559357fcaf0
Alve Svarén <alve@hotmail.se>
2020-12-02 06:25:27 +0100

    Formatting and made the code a bit cleaner

--- 2.py
+++ 2.py
@@ -5,14 +5,16 @@ data = aoc.get_input(2).splitlines()
 
 count1 = 0
 count2 = 0
-for password in data:
-    minc, maxc, letter, passwd = match = re.match(
-        r"(\d+)-(\d+) ([a-z]): ([a-z]+)", password).groups()
 
-    if passwd.count(letter) >= int(minc) and passwd.count(letter) <= int(maxc):
+for password in data:
+    minc, maxc, letter, passwd = re.match(
+        r"(\d+)-(\d+) ([a-z]): ([a-z]+)", password).groups()
+    minc, maxc = int(minc), int(maxc)
+    
+    if passwd.count(letter) >= minc and passwd.count(letter) <= maxc:
         count1 += 1
     
-    if ((passwd[int(minc)-1] == letter) is not (passwd[int(maxc)-1] == letter)):
+    if ((passwd[minc-1] == letter) is not (passwd[maxc-1] == letter)):
         count2 += 1
 
 print("Part 1:", count1)

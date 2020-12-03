e42d0ee6e5105e467a21b8902f493eb762700c27
Alve Svarén <alve@hotmail.se>
2020-12-03 07:01:27 +0100

    Made the regex shorter and now uses a boolean xor instead of is not

--- 2.py
+++ 2.py
@@ -7,13 +7,13 @@ count1, count2 = 0, 0
 
 for password in data:
     minc, maxc, letter, passwd = re.match(
-        r"(\d+)-(\d+) ([a-z]): ([a-z]+)", password).groups()
+        r"(\d+)-(\d+) (\w): (\w+)", password).groups()
     minc, maxc = int(minc), int(maxc)
 
-    if passwd.count(letter) >= minc and passwd.count(letter) <= maxc:
+    if maxc >= passwd.count(letter) >= minc:
         count1 += 1
 
-    if (passwd[minc-1] == letter) is not (passwd[maxc-1] == letter):
+    if (passwd[minc-1] == letter) ^ (passwd[maxc-1] == letter):
         count2 += 1
 
 print("Part 1:", count1)

22b0c1682b25715c4174529e3b55c4f5332ac74f
Alve Svarén <alve@hotmail.se>
2020-12-03 06:55:23 +0100

    Optimized the code a bit and merged the loops

--- 1.py
+++ 1.py
@@ -2,18 +2,16 @@ import aoc
 
 data = {*map(int, aoc.get_input(1).splitlines())}
 
-def part1():
-    for num1 in data:
+val1, val2 = 0, 0
+for num1 in data:
+    if not (val1 and val2):
         for num2 in data:
+            if not val2:
+                for num3 in data:
+                    if num1 + num2 + num3 == 2020:
+                        val2 = num1 * num2 * num3
             if num1 + num2 == 2020:
-                return num1 * num2
+                val1 = num1 * num2
 
-def part2():
-    for num1 in data:
-        for num2 in data:
-            for num3 in data:
-                if num1 + num2 + num3 == 2020:
-                    return num1 * num2 * num3
-
-print("Part 1:", part1())
-print("Part 2:", part2())
+print("Part 1:", val1)
+print("Part 2:", val2)

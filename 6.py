7c306f042603ee946d0330fde554f16f4e1b8708
Alve Svarén <alve@hotmail.se>
2020-12-06 06:15:30 +0100

    Added day 6

---
+++ 6.py
@@ -0,0 +1,23 @@
+import aoc
+
+data = aoc.get_input(6).split("\n\n")
+
+sum1, sum2 = 0, 0
+for group in data:
+    anyone_answered = set()
+    each_answer, people = [], 0
+    for person in group.splitlines():
+        people += 1
+        for question in person:
+            anyone_answered.add(question)
+            each_answer.append(question)
+    sum1 += len(anyone_answered)
+
+    checked = set()
+    for answer in each_answer:
+        if (not answer in checked) and each_answer.count(answer) == people:
+            sum2 += 1
+        checked.add(answer)
+
+print("Part 1:", sum1)
+print("Part 2:", sum2)

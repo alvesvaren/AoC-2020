2dbd84256b98201e88e0821d6418275c8d43ac0c
Alve Svarén <alve@hotmail.se>
2020-12-09 06:37:52 +0100

    Formatting and added part text

--- 9.py
+++ 9.py
@@ -11,10 +11,9 @@ for end in range(length, len(data)):
         for num2 in data[end-length:end]:
             if num1+num2 == to_sum and num1 != num2:
                 any_matches = True
-    
 
     if not any_matches:
-        print(to_sum)
+        print("Part 1:", to_sum)
         sum2, starti, endi = 0, 0, 0
         largest_range = []
         for starti in range(0, end):
@@ -22,15 +21,12 @@ for end in range(length, len(data)):
             sum2 = 0
             while sum2 < to_sum and i < end:
                 sum2 += data[i]
-                print(i, sum2)
-                
                 i += 1
             if sum2 != to_sum:
                 continue
             range_thing = range(starti, i-1)
-            if len(range_thing)  > len(largest_range):
+            if len(range_thing) > len(largest_range):
                 largest_range = range_thing
-        print(largest_range)
         data_range = data[largest_range.start:largest_range.stop]
-        print(min(data_range) + max(data_range))
+        print("Part 2:", min(data_range) + max(data_range))
         break

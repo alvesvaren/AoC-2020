9a8f045d5afeb6415715de9e165aa914c150fada
Alve Svarén <alve@hotmail.se>
2020-12-10 07:53:27 +0100

    Added part 2 solution

--- 10.py
+++ 10.py
@@ -1,4 +1,5 @@
 import aoc
+from functools import lru_cache
 from collections import defaultdict
 
 data = *map(int, aoc.get_input(10).splitlines()),
@@ -9,9 +10,16 @@ for adapter, other_adapter in zip(data, data[1:]):
     differences[abs(other_adapter - adapter)] += 1
 
 
-for adapter in data:
-    pass
-
+@lru_cache
+def count_combinations(start):
+    if start == data[-1]:
+        return 1
+    count = 0
+    for i in 1, 2, 3:
+        if start + i not in data:
+            continue
+        count += count_combinations(start + i)
+    return count
 
 print("Part 1:", differences[1] * differences[3])
-print("Part 2:")
+print("Part 2:", count_combinations(0))

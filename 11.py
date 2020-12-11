339e845c1f91a984685641e326910411a4addd4d
Alve Svarén <alve@hotmail.se>
2020-12-11 06:48:58 +0100

    Added solution for day 11

---
+++ 11.py
@@ -0,0 +1,91 @@
+import aoc
+from copy import copy
+
+data = aoc.get_input(11).splitlines()
+
+offsets = [
+    (1, 1),
+    (1, 0),
+    (1, -1),
+    (0, 1),
+    (0, -1),
+    (-1, 1),
+    (-1, 0),
+    (-1, -1)
+]
+
+seatmap = {}
+maxx, maxy = 0, 0
+for y, line in enumerate(data):
+    maxy = y + 1
+    for x, char in enumerate(line):
+        seatmap[x, y] = char
+        maxx = x + 1
+prev_state = copy(seatmap)
+
+
+def get_around(x: int, y: int) -> int:
+    adjacent_count = 0
+    for offset in offsets:
+        try:
+            if prev_state[x + offset[0], y + offset[1]] == "#":
+                adjacent_count += 1
+        except KeyError:
+            pass
+    return adjacent_count
+
+
+def get_visible(x: int, y: int) -> int:
+    visible_count = 0
+    for offset in offsets:
+        offset_offset = 1
+        for i in range(1, max(maxx, maxy)):
+            thing = offset[0] * i, offset[1] * i
+            try:
+                value = prev_state[x + thing[0], y + thing[1]]
+            except KeyError:
+                break
+            if value == "#":
+                visible_count += 1
+                break
+            elif value == "L":
+                break
+    return visible_count
+
+
+def step(visibility_method, maxcount) -> bool:
+    anything_changed = False
+    for y in range(maxy):
+        for x in range(maxx):
+            if prev_state[x, y] == "L" and visibility_method(x, y) == 0:
+                seatmap[x, y] = "#"
+                anything_changed = True
+            elif prev_state[x, y] == "#" and visibility_method(x, y) >= maxcount:
+
+                seatmap[x, y] = "L"
+                anything_changed = True
+    return anything_changed
+
+
+start_map = copy(seatmap)
+while step(get_around, 4):
+    prev_state = copy(seatmap)
+
+count = 0
+for y in range(maxy):
+    for x in range(maxx):
+        if seatmap[x, y] == "#":
+            count += 1
+
+seatmap = start_map
+while step(get_visible, 5):
+    prev_state = copy(seatmap)
+
+count2 = 0
+for y in range(maxy):
+    for x in range(maxx):
+        if seatmap[x, y] == "#":
+            count2 += 1
+
+print("Part 1:", count)
+print("Part 2:", count2)

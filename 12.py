d7e44ca642917e208c9fe7a708afa1ef706f9262
Alve Svarén <alve@hotmail.se>
2020-12-12 06:41:40 +0100

    Part 2 almost works

--- 12.py
+++ 12.py
@@ -2,11 +2,13 @@ import aoc,re
 
 data = aoc.get_input(12).splitlines()
 
-e,n,r = 0, 0, 0
-we, wn = 1, 10
+n,e,r = 0, 0, 0
+e2, n2 = 0, 0
+we, wn = 10, 1
 for instruction in data:
     direction, value = re.match(r"([NSFWELR])(\d+)", instruction).groups()
     value = int(value)
+    orig_direction = direction
     
     if direction == "F":
         r = r % 4
@@ -18,17 +20,47 @@ for instruction in data:
             direction = "S"
         elif r == 0:
             direction = "E"
+        
+        e2 += we * value
+        n2 += wn * value
+
     if direction == "R":
         r -= value//90
     elif direction == "L":
         r += value//90
     elif direction == "N":
-        e += value
-    elif direction == "S":
-        e -= value
-    elif direction == "E":
         n += value
-    elif direction == "W":
+    elif direction == "S":
         n -= value
+    elif direction == "E":
+        e += value
+    elif direction == "W":
+        e -= value
     
+    if orig_direction == "R":
+        if value == 90:
+            we, wn = wn, -we
+        elif value == 270:
+            we, wn = -wn, we
+        else:
+            we, wn = -wn, -we
+    elif orig_direction == "L":
+        if value == 90:
+            we, wn = -wn, we
+        elif value == 270:
+            we, wn = wn, -we
+        else:
+            we, wn = -wn, -we
+    elif orig_direction == "N":
+        wn += value
+    elif orig_direction == "S":
+        wn -= value
+    elif orig_direction == "E":
+        we += value
+    elif orig_direction == "W":
+        we -= value
+    # print(instruction, e, n, r)
+    print(instruction, we, wn, e2, n2)
+
 print(abs(e)+abs(n))
+print(abs(e2)+abs(n2))

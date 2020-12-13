78b21f32b4d7684f1c9715d8eb402dafa1d4da61
Alve Svarén <alve@hotmail.se>
2020-12-13 07:02:32 +0100

    I have no idea at all to why this works but it does

--- 13.py
+++ 13.py
@@ -1,7 +1,5 @@
 import aoc
-import sys
-from functools import lru_cache
-
+from sympy.ntheory.modular import crt
 arrival, schedule = aoc.get_input(13).splitlines()
 
 arrival = int(arrival)
@@ -17,5 +15,14 @@ for bus in busses:
     if not minimum_bus or thing < minimum_bus[1]:
         minimum_bus = (bus, thing)
 
+# https://www.geeksforgeeks.org/python-sympy-crt-method/ heh
+m = []
+v = []
+for i, bus in enumerate(busses):
+    if bus != "x":
+        bus = int(bus)
+        m.append(bus)
+        v.append(bus - i)
 
-print(minimum_bus[0]*(minimum_bus[1]-arrival))
+print("Part 1:", minimum_bus[0]*(minimum_bus[1]-arrival))
+print("Part 2:", crt(m, v)[0])

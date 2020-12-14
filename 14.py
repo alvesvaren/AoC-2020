eaf6647c7a5c6628de9c69d62f6db4eabd7e956b
Alve Svarén <alve@hotmail.se>
2020-12-14 07:54:09 +0100

    Moved function out of thing

--- 14.py
+++ 14.py
@@ -5,26 +5,28 @@ from collections import defaultdict
 
 data = *map(lambda x: x.split(" = "), aoc.get_input(14).splitlines()),
 
+
+def gen_addresses(start_address, new_mask):
+    if new_mask:
+        for curr_address in gen_addresses(start_address//2, new_mask[:-1]):
+            current_bit = new_mask[-1]
+            curr_address *= 2
+            if current_bit == "0":
+                yield curr_address + start_address % 2
+            if current_bit in "1X":
+                yield curr_address + 1
+            if current_bit == "X":
+                yield curr_address
+    else:
+        yield 0
+
+
 mem1, mem2 = defaultdict(int), defaultdict(int)
 mask = "", ""
 for instruction, value in data:
     if instruction.startswith("mem"):
         address1 = int(re.split("[\[\]]", instruction)[1])
         value = int(value)
-
-        def gen_addresses(start_address, new_mask):
-            if new_mask:
-                for curr_address in gen_addresses(start_address//2, new_mask[:-1]):
-                    current_bit = new_mask[-1]
-                    curr_address *= 2
-                    if current_bit == "0":
-                        yield curr_address + start_address % 2
-                    if current_bit in "1X":
-                        yield curr_address + 1
-                    if current_bit == "X":
-                        yield curr_address
-            else:
-                yield 0
         for address in gen_addresses(address1, mask):
             mem2[address] = value
         value |= int(mask.replace("X", "0"), 2)

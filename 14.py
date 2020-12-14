ebb012d2ceae26e79cc82ebff35035e7749c64d9
Alve Svarén <alve@hotmail.se>
2020-12-14 07:51:46 +0100

    Complete rework of part 2 that works

--- 14.py
+++ 14.py
@@ -7,28 +7,31 @@ data = *map(lambda x: x.split(" = "), aoc.get_input(14).splitlines()),
 
 mem1, mem2 = defaultdict(int), defaultdict(int)
 mask = "", ""
-for instruction, value1 in data:
+for instruction, value in data:
     if instruction.startswith("mem"):
         address1 = int(re.split("[\[\]]", instruction)[1])
-        value1 = int(value1)
-        value2 = value1
-        flip = False
+        value = int(value)
 
-        def gen_addresses(base_mask):
-            address = 0
-            while 1:
-                yield address
-                address += 1
-                address = address + ~base_mask & base_mask
-                if address == 0:
-                    break
-        for address2 in gen_addresses(int(mask.replace("1", "0").replace("X", "1"), 2)):
-            mem2[address2] = value2
-        value1 |= int(mask.replace("X", "0"), 2)
-        value1 &= int(mask.replace("X", "1"), 2)
-        mem1[address1] = int(value1)
+        def gen_addresses(start_address, new_mask):
+            if not new_mask:
+                yield 0
+            else:
+                for curr_address in gen_addresses(start_address//2, new_mask[:-1]):
+                    current_bit = new_mask[-1]
+                    curr_address *= 2
+                    if current_bit == "0":
+                        yield curr_address + start_address % 2
+                    if current_bit in "1X":
+                        yield curr_address + 1
+                    if current_bit == "X":
+                        yield curr_address
+        for address in gen_addresses(address1, mask):
+            mem2[address] = value
+        value |= int(mask.replace("X", "0"), 2)
+        value &= int(mask.replace("X", "1"), 2)
+        mem1[address1] = int(value)
     elif instruction == "mask":
-        mask = value1
+        mask = value
 
 print("Part 1:", sum(mem1.values()))
 print("Part 2:", sum(mem2.values()))

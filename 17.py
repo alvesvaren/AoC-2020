84c4e21b2e808894293b68f040f38a836b937bea
Alve Svarén <alve@hotmail.se>
2020-12-17 07:01:53 +0100

    Formatting and replaced some reoccuring code with two functions

--- 17.py
+++ 17.py
@@ -30,34 +30,28 @@ prev_space3d = copy(space3d)
 prev_space4d = copy(space4d)
 
 
-def get_maxes3d():
-    maxx = max(space3d.keys(), key=itemgetter(0))[0] + 1
-    maxy = max(space3d.keys(), key=itemgetter(1))[1] + 1
-    maxz = max(space3d.keys(), key=itemgetter(2))[2] + 1
-    return maxx, maxy, maxz
+def get_maxes(four: bool):
+    space = space4d if four else space3d
+    maxx = max(space.keys(), key=itemgetter(0))[0] + 1
+    maxy = max(space.keys(), key=itemgetter(1))[1] + 1
+    maxz = max(space.keys(), key=itemgetter(2))[2] + 1
+    thing = maxx, maxy, maxz
+    if four:
+        maxw = max(space.keys(), key=itemgetter(3))[3] + 1
+        thing = *thing, maxw
+    return thing
 
 
-def get_mins3d():
-    miny = min(space3d.keys(), key=itemgetter(1))[1]
-    minx = min(space3d.keys(), key=itemgetter(0))[0]
-    minz = min(space3d.keys(), key=itemgetter(2))[2]
-    return minx, miny, minz
-
-
-def get_maxes4d():
-    maxx = max(space4d.keys(), key=itemgetter(0))[0] + 1
-    maxy = max(space4d.keys(), key=itemgetter(1))[1] + 1
-    maxz = max(space4d.keys(), key=itemgetter(2))[2] + 1
-    maxw = max(space4d.keys(), key=itemgetter(3))[3] + 1
-    return maxx, maxy, maxz, maxw
-
-
-def get_mins4d():
-    minx = min(space4d.keys(), key=itemgetter(0))[0]
-    miny = min(space4d.keys(), key=itemgetter(1))[1]
-    minz = min(space4d.keys(), key=itemgetter(2))[2]
-    minw = min(space4d.keys(), key=itemgetter(3))[3]
-    return minx, miny, minz, minw
+def get_mins(four: bool):
+    space = space4d if four else space3d
+    minx = min(space.keys(), key=itemgetter(0))[0]
+    miny = min(space.keys(), key=itemgetter(1))[1]
+    minz = min(space.keys(), key=itemgetter(2))[2]
+    thing = minx, miny, minz
+    if four:
+        minw = min(space.keys(), key=itemgetter(3))[3]
+        thing = *thing, minw
+    return thing
 
 
 def get_cubes4d(x, y, z, w) -> int:
@@ -78,10 +72,10 @@ def get_cubes3d(x, y, z) -> int:
 
 def step3d(maxstep=6):
     global prev_space3d
-    for i in range(maxstep):
+    for step in range(maxstep):
         prev_space3d = copy(space3d)
-        maxx, maxy, maxz = get_maxes3d()
-        minx, miny, minz = get_mins3d()
+        maxx, maxy, maxz = get_maxes(False)
+        minx, miny, minz = get_mins(False)
         for x in range(minx - 1, maxx + 1):
             for y in range(miny - 1, maxy + 1):
                 for z in range(minz - 1, maxz + 1):
@@ -96,26 +90,22 @@ def step3d(maxstep=6):
 
 def step4d(maxstep=6):
     global prev_space4d
-    for i in range(maxstep):
+    for step in range(maxstep):
         prev_space4d = copy(space4d)
-        maxx4d, maxy4d, maxz4d, maxw4d = get_maxes4d()
-        minx4d, miny4d, minz4d, minw4d = get_mins4d()
-        for x in range(minx4d - 1, maxx4d + 1):
-            for y in range(miny4d - 1, maxy4d + 1):
-                for z in range(minz4d - 1, maxz4d + 1):
-                    for w in range(minw4d - 1, maxw4d + 1):
+        maxx, maxy, maxz, maxw = get_maxes(True)
+        minx, miny, minz, minw = get_mins(True)
+        for x in range(minx - 1, maxx + 1):
+            for y in range(miny - 1, maxy + 1):
+                for z in range(minz - 1, maxz + 1):
+                    for w in range(minw - 1, maxw + 1):
                         cube = prev_space4d[x, y, z, w]
                         cubes = get_cubes4d(x, y, z, w)
                         if cube and not cubes in (2, 3):
                             space4d[x, y, z, w] = False
                         if not cube and cubes == 3:
                             space4d[x, y, z, w] = True
-        # pretty_print_space()
     return sum(1 if i else 0 for i in space4d.values())
 
 
-# space_copy = copy(space)
-# while step():
-#     prev_space = copy(space)
 print("Part 1:", step3d())
 print("Part 2:", step4d())
